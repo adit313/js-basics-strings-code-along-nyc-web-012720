4edaade888e25c82044f00ef48911196fbbcd5e3
adit313 <app2116@gmail.com>
2020-03-09 09:14:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+currentUser = "Adit"
+welcomeMessage = "Welcome to Flatbook, " + currentUser + '!'
+excitedWelcomeMessage = ("Welcome to Flatbook, " + currentUser + '!').toUpperCase()
+shortGreeting = "Welcome, " + currentUser[0] + '!'
